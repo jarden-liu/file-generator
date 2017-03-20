05f2011dc5debfffbc77905df042b88396bb67d8
jarden <372002582@qq.com>
2017-03-20 11:14:03 +0800

    update img

--- spec/file-generator-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FileGeneratorView from '../lib/file-generator-view';
-
-describe('FileGeneratorView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
